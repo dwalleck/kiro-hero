02ea7f78e8f5e8b86cbc8528277c6f59906cbf34
Daryl Walleck <dwalleck@proton.me>
2026-06-15 20:18:20 -0500

    Add dependencies for sst

---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "kiro-hero",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Astro("MyWeb");
+  },
+});
